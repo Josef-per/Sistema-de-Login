f685d4eba36bdf1ad59ce68a90e51e3414825e6a
José Francsico Silva Pereira <josef.per@proton.me>
2026-08-28 08:27:46 -0300

    feat: First deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //construção da váriavel que leva o nosso App.jsx para o index.html
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 
 import App from "./App";
 
@@ -11,7 +11,7 @@ import "./styles/global.css";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
